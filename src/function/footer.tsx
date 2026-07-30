4823eb542fdaedbafb3edeaf6469a58d0c2310ac
Lorenzo Cioffi <cioffilorenzo24@gmail.com>
2026-07-30 10:13:18 +0200

    updates

--- src/function/footer.tsx
+++ src/function/footer.tsx
@@ -3,31 +3,33 @@ import { GetAverage } from "./challengers";
 
 export function BetterChoises(currentDevA: Dev, currentDevB: Dev) {
   if (currentDevA === null || currentDevB === null)
-    return <p style={{ color: "white" }}>Seleziona gli sfidanti</p>;
+    return 
 
   const A = GetAverage(currentDevA);
   const B = GetAverage(currentDevB);
   let winnerClass = "";
   let winnerFE = "";
+  let winnerFEClass = "";
 
   if (A.averageFrontendScore > B.averageFrontendScore) {
     winnerFE = currentDevA.firstname + " " + currentDevA.lastname;
-    winnerClass = "winner-A";
+    winnerFEClass = "winner-A";
   } else if (A.averageFrontendScore < B.averageFrontendScore) {
     winnerFE = currentDevB.firstname + " " + currentDevB.lastname;
-    winnerClass = "winner-B";
+    winnerFEClass = "winner-B";
   } else {
     winnerFE = "entrambi";
   }
 
   let winnerBE = "";
+  let winnerBEClass = "";
 
   if (A.averageBeckendScore > B.averageBeckendScore) {
     winnerBE = currentDevA.firstname + " " + currentDevA.lastname;
-    winnerClass = "winner-A";
+    winnerBEClass = "winner-A";
   } else if (A.averageBeckendScore < B.averageBeckendScore) {
     winnerBE = currentDevB.firstname + " " + currentDevB.lastname;
-    winnerClass = "winner-B";
+    winnerBEClass = "winner-B";
   } else {
     winnerBE = "entrambi";
   }
@@ -69,12 +71,12 @@ export function BetterChoises(currentDevA: Dev, currentDevB: Dev) {
         );
       })}
       <div className="betterDev">
-        Per il miglior Front-End, scegli <span className={winnerClass}>{winnerFE}</span>
+        Per il miglior Front-End, scegli{" "}  <span className={winnerFEClass}>{winnerFE}</span>
       </div>
       <div className="betterDev">
-        Per il miglior Beck-End, scegli <span className={winnerClass}>{winnerBE}</span>
+        Per il miglior Beck-End, scegli{" "} <span className={winnerBEClass}>{winnerBE}</span>
       </div>
-      <div className="bestDev">Complessivamente, scegli <span className={winnerClass}>{BestDev}</span></div>
+      <div className="bestDev">Complessivamente, scegli{" "} <span className={winnerClass}>{BestDev}</span></div>
     </>
   );
 }
